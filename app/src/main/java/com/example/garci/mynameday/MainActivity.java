d15397b960f8db31a57413555f6d5261cb8a57bf
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2017-11-27 22:51:27 -0600

    Remove unused libraries and functions Modify text appearance Add comments to main activity

--- app/src/main/java/com/example/garci/mynameday/MainActivity.java
+++ app/src/main/java/com/example/garci/mynameday/MainActivity.java
@@ -8,29 +8,26 @@ import android.view.View;
 import android.app.DatePickerDialog;
 import android.widget.DatePicker;
 import android.widget.TextView;
-import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void datePicker (View view) {
+    /* Function that displays datepicker window when button is clicked */
+    public void showDatePicker (View view) {
         DatePickerFragment fragment = new DatePickerFragment();
         fragment.show(getSupportFragmentManager(), "date");
     }
 
-    private void setDate(final Calendar calendar) {
-        final DateFormat dateFormat  = DateFormat.getDateInstance(DateFormat.MEDIUM);
-    }
-
+    /* Function that populates textview with text regarding day of week user was born on */
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        Calendar calendar = new GregorianCalendar(year, month, day);
-        setDate(calendar);
+
+        /* Implement Zeller Algorithm*/
         month = month + 1;
         String dayOfTheWeek;
 
@@ -38,8 +35,9 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
             month = month + 12;
             year = year - 1;
         }
-
         int dayOfWeek = (day + ((13*(month + 1))/5) + (year%100) + ((year%100)/4) + ((year/100)/4) + 5*(year/100))%7;
+
+        /* Based on Zeller algorithm output, decides which day of the week date fell on*/
         if (dayOfWeek == 0) {
             dayOfTheWeek = "Saturday";
         }
@@ -71,18 +69,19 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         else {
             dayOfTheWeek = "ERROR";
         }
+
         ((TextView) findViewById(R.id.textview_birthdate_description)).setText("You were born on a " + dayOfTheWeek + "!");
     }
 
     public static class DatePickerFragment extends DialogFragment {
+
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             final Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
-
-            return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+            return new DatePickerDialog(getActivity(),(DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
         }
     }
 }
